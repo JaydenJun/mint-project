f554a329338cd1674d1e92184813f2feab44e709
zj <zj@tedu.cn>
2022-06-02 09:22:59 +0800

    完成注册

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,56 +1,67 @@
 <template>
   <div>
-    <van-nav-bar title="标题" left-text="返回" left-arrow>
-  <template #right>
-    <van-icon name="search" size="18" />
-  </template>
-</van-nav-bar>
-<van-form @submit="onSubmit">
-  <van-field
-    v-model="username"
-    name="用户名"
-    label="用户名"
-    placeholder="用户名"
-    :rules="[{ required: true, message: '请填写用户名' }]"
-  />
-  <van-field
-    v-model="password"
-    type="password"
-    name="密码"
-    label="密码"
-    placeholder="密码"
-    :rules="[{ required: true, message: '请填写密码' }]"
-  />
-  <div style="margin: 16px;">
-    <van-button round block type="info" native-type="submit">提交</van-button>
-  </div>
-</van-form>
-<van-goods-action>
-  <van-goods-action-icon icon="chat-o" text="客服" dot />
-  <van-goods-action-icon icon="cart-o" text="购物车" badge="5" />
-  <van-goods-action-icon icon="shop-o" text="店铺" badge="12" />
-  <van-goods-action-button type="warning" text="加入购物车" />
-  <van-goods-action-button type="danger" text="立即购买" />
-</van-goods-action>
+    <mt-header title="登录">
+      <mt-button icon="back" slot="left"></mt-button>
+      <router-link to="/register" slot="right">新用户</router-link>
+    </mt-header>
+    <mt-field
+      v-model="name"
+      :state="nameState"
+      type="text"
+      label="用户名"
+      placeholder="用输入用户名"
+      @blur.native.capture="checkName"
+    ></mt-field>
+    <mt-field
+      v-model="pwd"
+      :state="pwdState"
+      type="password"
+      label="密码"
+      placeholder="用输入密码"
+      @blur.native.capture="checkPwd"
+    ></mt-field>
+    <mt-button type="primary" size="large" @click="login">安全登录</mt-button>
   </div>
 </template>
-
 <script>
-  export default {
-     data() {
+export default {
+  data() {
     return {
-      username: '',
-      password: '',
+      name: "",
+      nameState: "",
+      pwdState: "",
+      pwd: "",
     };
   },
   methods: {
-    onSubmit(values) {
-      console.log('submit', values);
+    login() {
+      if (this.checkName() && this.checkPwd()) {
+        console.log(`${this.name},${this.pwd}`);
+      }
+    },
+    checkName() {
+      let reg = /^\w{3,15}$/;
+      if (reg.test(this.name)) {
+        this.nameState = "success";
+        return true;
+      } else {
+        this.nameState = "error";
+        return false;
+      }
+      console.log("用户名:" + this.name);
+    },
+    checkPwd() {
+      let reg = /^\d{6}$/;
+      if (reg.test(this.pwd)) {
+        this.pwdState = "success";
+        return true;
+      } else {
+        this.pwdState = "error";
+        return false;
+      }
     },
   },
-  }
+};
 </script>
-
 <style lang="scss" scoped>
-
 </style>
